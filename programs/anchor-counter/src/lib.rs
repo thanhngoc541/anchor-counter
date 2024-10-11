f87d965cc414667a2fb9a6a9a4efc538be7984fb
ngoc <ngochc1@gmail.com>
2024-10-11 16:52:36 +0700

    Base

---
+++ programs/anchor-counter/src/lib.rs
@@ -0,0 +1,57 @@
+use anchor_lang::prelude::*;
+
+declare_id!("9R5tjTKgM49xKCD6B6nJrYDidbqo3SBwaorNmMRZ7j4y");
+
+#[program]
+pub mod anchor_counter {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        msg!("Counter Account Created");
+        msg!("Current Count: { }", counter.count);
+        Ok(())
+    }
+    pub fn increment(ctx: Context<Update>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter incremented. Current count: {}", counter.count);
+        Ok(())
+    }
+    pub fn decrement(ctx: Context<Update>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+        counter.count = counter.count.checked_sub(1).unwrap();
+        msg!("Counter decremented. Current count: {}", counter.count);
+        Ok(())
+    }
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Counter {
+    pub count: u64,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+}
+
+const DISCRIMINATOR: usize = 8;
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init,
+        payer = user,
+        space = DISCRIMINATOR + Counter::INIT_SPACE
+    )]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
